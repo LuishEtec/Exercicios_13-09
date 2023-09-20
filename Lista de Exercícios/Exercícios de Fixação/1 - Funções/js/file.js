8a656198739fe824e527834e7b7e6b50702c7f20
LuishEtec <142437702+LuishEtec@users.noreply.github.com>
2023-09-20 13:33:10 -0300

    Add files via upload

---
+++ Lista de Exercícios/Exercícios de Fixação/1 - Funções/js/file.js
@@ -0,0 +1,14 @@
+function olaMundo(){
+    alert("Olá Mundo");
+}
+
+function mostrarNome(){
+    let nome1 = prompt("Digite seu nome");
+    console.log(`${nome1}`);
+}
+
+function concatenaPalavras(){
+    let palavra1 = prompt("Digite uma palavra");
+    let palavra2 = prompt("Digite outra palavra");
+    console.log(`Palavras concatenadas: ${palavra1} ${palavra2}`);
+}
